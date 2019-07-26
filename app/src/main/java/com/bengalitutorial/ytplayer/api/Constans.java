28d67ddf43287b238ea8e07d1024b8d9da9d248d
prosenjitcode <52832618+prosenjitcode@users.noreply.github.com>
2019-07-26 10:22:42 +0530

    Update Constans.java

--- app/src/main/java/com/bengalitutorial/ytplayer/api/Constans.java
+++ app/src/main/java/com/bengalitutorial/ytplayer/api/Constans.java
@@ -3,7 +3,7 @@ package com.bengalitutorial.ytplayer.api;
 public class Constans {
 
     public static final String BASE_URL = "https://www.googleapis.com/youtube/v3/";
-    public static final String API_KEY = "AIzaSyBQwuUcWE9qugNoxzcWUEsWs8iVNAdn2_Q";
+    public static final String API_KEY = "youtube api key";
 
 
 }
